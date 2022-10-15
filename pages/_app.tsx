e74f22ad9fc8195663d33fea4edc9c5690b97f65
Karl Hadwen <karlhadwen@gmail.com>
2022-10-15 12:02:37 +0100

    chore: first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,5 @@
+export default function Home(): JSX.Element {
+  return (
+    <p>Hello</p>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+      <Component {...pageProps} />
+  );
+}
+
+export default MyApp;
